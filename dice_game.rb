3b1a2fa2cba8e907319a59f97587e39c76364748
SimonBao <Simonbao.sb@gmail.com>
2017-11-30 16:57:13 +0000

    Added alternative code to dice_game.rb

--- dice_game.rb
+++ dice_game.rb
@@ -1,3 +1,4 @@
+=begin 
 def rand_number
   rand(6)+1
 end
@@ -16,6 +17,27 @@ def dice_game
 end
 puts "Game Ended! Scores are \n Player 1: Won #{player1_score} Round \n Player 2: Won #{player2_score} Round"
 puts player1_score > player2_score ? "Congratulations Player 1, You Won!" : "Congratulations Player 2, You Won!"
+end
+=end
+
+#Above is the origin code, bottom is alternative.
+
+def rand_number
+  rand(6) + 1
+end
+
+def dice_game
+  player1_score = 0
+  player2_score = 0
+  3.times do
+    p1 = rand_number
+    p2 = rand_number
+    p1 == p2 ? nil : p1 > p2 ? player1_score += 1 : player2_score += 1
+  end
+  puts "Game Ended! Scores are \n Player 1: Won #{player1_score} Round \n Player 2: Won #{player2_score} Round\n" +
+   (player1_score == player2_score ? "It's a DRAW!" :
+   	player1_score > player2_score ? 'Congratulations Player 1, You Won!' :
+   'Congratulations Player 2, You Won!')
 end
 
 dice_game
